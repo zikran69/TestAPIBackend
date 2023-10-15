339abe4cefa3bffc342ddc7e65722e0770df55cf
rizqiau <rizqiau@mochammadrizqiaullia@gmail.com>
2023-10-15 22:42:04 +0700

    mengerjakan API room

--- src/module/room/room.controller.js
+++ src/module/room/room.controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { allUsers } = require("./room.service");
+const { allUsers, createRoom, deleteRoomById, roomById, editRoomById } = require("./room.service");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
@@ -9,4 +9,65 @@ router.get("/", async (req, res) => {
   res.send(users);
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const newData = req.body;
+
+
+    const room = await createRoom(newData);
+
+    res.send({
+      data: room,
+      message: "Create room success",
+    });
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const roomId = req.params.id;
+  const newData = req.body;
+  if (
+    !(
+      newData.categoryId &&
+      newData.floorId &&
+      newData.nameRoom &&
+      newData.numberRoom &&
+      newData.statusId &&
+      newData.descRoom 
+    )
+  ) {
+    return res.status(400).send("some fields are missings");
+  }
+  const room = await editRoomById(parseInt(roomId), newData);
+
+  res.send({
+    data: room,
+    message: "Update Room success",
+  });
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const roomId = req.params.id;
+    await deleteRoomById(parseInt(roomId));
+
+    res.send("Room deleted");
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const roomId = parseInt(req.params.id);
+    const room = await roomById(roomId);
+    res.send(room);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
 module.exports = router;
+
