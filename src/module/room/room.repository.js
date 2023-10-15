f00483c2d8e5843f5d9a5ccb655dabf39cbfae9d
Kelompok-5 <142487224+bootcamp-8@users.noreply.github.com>
2023-10-15 23:59:04 +0700

    master (#11)
    * mengerjakan API room
    
    * Hilmi (#10)
    
    * perbaikan model user
    
    * Delete asset/image/admin/poto.png
    
    Signed-off-by: Kelompok-5 <142487224+bootcamp-8@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Kelompok-5 <142487224+bootcamp-8@users.noreply.github.com>
    Co-authored-by: Hilmi <hilmi@amtiss.com>
    
    ---------
    
    Signed-off-by: Kelompok-5 <142487224+bootcamp-8@users.noreply.github.com>
    Co-authored-by: rizqiau <rizqiau@mochammadrizqiaullia@gmail.com>
    Co-authored-by: Hilmi <hilmi@amtiss.com>

--- src/module/room/room.repository.js
+++ src/module/room/room.repository.js
@@ -9,11 +9,77 @@ const findAllUser = async () => {
       nameRoom: true,
       statusId: true,
       descRoom: true,
+      Status: true,
+      Floor: true,
+      Category: true,
+      numberRoom: true,
     },
   });
   return users;
 };
 
+const findRoom = async (id) => {
+  const room = await prisma.room.findUnique({
+    where: {
+      idRoom: id,
+    },
+  });
+  return room;
+};
+
+const checkRoom = async (newData) => {
+  const rooms = await prisma.room.findFirst({
+    where: {      
+      numberRoom: newData.numberRoom,
+    },
+  });
+  return rooms;
+};
+
+const insertRoom = async (newData) => {
+  const room = await prisma.room.create({
+    data: {
+      categoryId: newData.categoryId,
+      floorId: newData.floorId,
+      nameRoom: newData.nameRoom,
+      numberRoom: newData.numberRoom,
+      statusId: newData.statusId,
+      descRoom: newData.descRoom,
+    },
+  });
+  return room;
+};
+
+const updateRoom = async (id, newData) => {
+  const room = await prisma.room.update({
+    where: {
+      idRoom: id,
+    },
+    data: {
+      categoryId: newData.categoryId,
+      floorId: newData.floorId,
+      nameRoom: newData.nameRoom,
+      numberRoom: newData.numberRoom,
+      statusId: newData.statusId,
+      descRoom: newData.descRoom,
+    },
+  });
+  return room;
+};
+
+const deleteRoom = async (id) => {
+  await prisma.room.delete({
+    where: {
+      idRoom: id,
+    },
+  });
+};
+
 module.exports = {
   findAllUser,
+  checkRoom,
+  insertRoom,
+  deleteRoom,
+  findRoom,
+  updateRoom,
 };
--- src/module/room/room.service.js
+++ src/module/room/room.service.js
@@ -1,12 +1,48 @@
 const express = require("express");
 
-const { findAllUser } = require("./room.repository");
+const { findAllUser, insertRoom, checkRoom, deleteRoom, findRoom, updateRoom } = require("./room.repository");
 
 const allUsers = async () => {
   const users = await findAllUser();
   return users;
 };
 
+const roomById = async (id) => {
+  const room = await findRoom(id);
+  if (!room) {
+    throw Error("Room not found");
+  }
+  return room;
+};
+
+const createRoom = async (newData) => {
+  const check = await checkRoom(newData);
+  if (check) {
+    throw Error("Room already exists");
+  }
+  const room = await insertRoom(newData);
+
+  return room;
+};
+
+const editRoomById = async (id, newData) => {
+  await roomById(id);
+
+  const room = await updateRoom(id, newData);
+
+  return room;
+};
+
+const deleteRoomById = async (id) => {
+  await roomById(id);
+
+  await deleteRoom(id);
+};
+
 module.exports = {
   allUsers,
+  createRoom,
+  deleteRoomById,
+  roomById,
+  editRoomById,
 };
