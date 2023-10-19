6719ab8ca92707dd5ed223f912a04d1313474ce6
zikran69 <108022748+zikran69@users.noreply.github.com>
2023-10-20 00:02:42 +0700

    Update (#16)
    * build booking modul
    
    * menambahkan POST method pada booking, untuk input 2 tabel dalam 1 POST

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../module/admin/admin.controller");
 const roomController = require("../module/room/room.controller");
 const floorController = require("../module/floor/floor.controller");
+const bookingController = require("../module/booking/booking.controller");
 const loginController = require("../module/login/login.controller");
 const { ValidateLogin } = require("../middlewares/validator");
 
@@ -10,5 +11,6 @@ router.use("/login", ValidateLogin, loginController);
 router.use("/users", userController);
 router.use("/rooms", roomController);
 router.use("/floor", floorController);
+router.use("/booking", bookingController);
 
 module.exports = router;
