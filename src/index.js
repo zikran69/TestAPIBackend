d5788f6c149c31717239c5e45695fb2893f071e6
bahe <bahebdg@gmail.com>
2023-10-22 01:29:32 +0700

    mengerjakan backend category

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const dotenv = require("dotenv");
+require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const router = require("./routes/router");
-dotenv.config();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(
--- src/routes/router.js
+++ src/routes/router.js
@@ -8,11 +8,25 @@ const loginController = require("../module/login/login.controller");
 const reportController = require("../module/report/report.controller");
 const { ValidateLogin } = require("../middlewares/validator");
 
+const {
+  addCategory,
+  allCategory,
+  categoryId,
+  categorySearch,
+  categoryDelete,
+} = require("../module/category");
+
 router.use("/login", ValidateLogin, loginController);
 router.use("/users", userController);
 router.use("/rooms", roomController);
 router.use("/floor", floorController);
 router.use("/booking", bookingController);
 router.use("/reports", reportController);
+
+router.use("/category-add", addCategory);
+router.use("/categories", allCategory);
+router.use("/category/:id", categoryId);
+router.use("/category-search/:name", categorySearch);
+router.use("/category-delete/:id", categoryDelete);
 
 module.exports = router;
