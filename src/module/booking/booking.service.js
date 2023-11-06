ec0ec7b6c1e140f0506ec8021d17a2cbbbac20de
zikran69 <zikzigi69@gmail.com>
2023-11-06 23:07:28 +0700

    menambah body email

--- src/module/booking/booking.service.js
+++ src/module/booking/booking.service.js
@@ -1,4 +1,7 @@
+const express = require("express");
 const nodemailer = require("nodemailer");
+const handlebars = require("handlebars");
+const fs = require("fs");
 const {
   checkUser,
   insertUser,
@@ -6,6 +9,8 @@ const {
 } = require("./booking.repository");
 
 const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.email",
+  secure: false,
   service: "gmail",
   auth: {
     user: "trianahilmi7@gmail.com",
@@ -22,11 +27,22 @@ const createUser = async (newData) => {
     var cust = custInsert.idCustomer;
   }
   const user = await insertUser(newData, cust);
+  const source = fs.readFileSync("email_template.html", "utf-8").toString();
+  const template = handlebars.compile(source);
+  const replacements = {
+    username: "zikran",
+  };
+  const htmlToSend = template(replacements);
+
   const mailOpstions = {
+    // email admin operational
     from: "trianahilmi7@gmail.com",
+    // email customer
     to: "trianahilmi5@gmail.com",
-    subject: "Node Mailer TEST",
-    text: "Test Sending Email Using NodeJs",
+
+    //body email :
+    subject: "your hotel booking successfuly verifited ",
+    html: htmlToSend,
   };
 
   transporter.sendMail(mailOpstions, function (err, info) {
