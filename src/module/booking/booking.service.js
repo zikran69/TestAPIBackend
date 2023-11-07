4946420a20a4d06dbaaba5040fd43e0019710ef4
zikran69 <zikzigi69@gmail.com>
2023-11-07 22:21:38 +0700

    revisi body email & menambahkan method GET untuk API page laporan

--- src/module/booking/booking.service.js
+++ src/module/booking/booking.service.js
@@ -27,18 +27,88 @@ const createUser = async (newData) => {
     var cust = custInsert.idCustomer;
   }
   const user = await insertUser(newData, cust);
-  const source = fs.readFileSync("email_template.html", "utf-8").toString();
-  const template = handlebars.compile(source);
-  const replacements = {
-    username: "zikran",
-  };
-  const htmlToSend = template(replacements);
+  const htmlToSend = `<!DOCTYPE html>
+  <html>
+    <head>
+      <style>
+        body {
+          font-family: Arial, sans-serif;
+          background-color: #f2f2f2;
+        }
+  
+        .container {
+          max-width: 600px;
+          margin: 0 auto;
+          padding: 20px;
+          background-color: #fff;
+          border-radius: 5px;
+          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        }
+  
+        .header {
+          text-align: center;
+          margin-bottom: 20px;
+        }
+  
+        .header h1 {
+          color: #333;
+          font-size: 24px;
+          margin: 0;
+        }
+  
+        .content {
+          margin-bottom: 30px;
+        }
+  
+        .content p {
+          margin: 0 0 10px;
+          line-height: 1.5;
+        }
+  
+        .footer {
+          text-align: center;
+        }
+  
+        .footer p {
+          color: #999;
+          font-size: 14px;
+          margin: 0;
+        }
+      </style>
+    </head>
+    <body>
+      <div class="container">
+        <div class="header">
+          <h1>Hellow Customer!</h1>
+          <h3>Berikut kami kirimkan bukti pemesanan Booking hotel Anda</h3>
+        </div>
+        <div class="content">
+          <p>nama : ${newData.nameCustomer}</p>
+          <p>Tanggal booking : ${new Date().toDateString()}.</p>
+          <p>
+            Silahkan datang ke hotel kami dan tunjukan
+            <strong>BUKTI BOOKING</strong> ini, pada receptionist yang bertugas di
+            sana.
+          </p>
+          <p>
+            Terima Kasih sudah mempergunakan layanan kami, selamat menikmati
+            liburan anda.
+          </p>
+        </div>
+        <div class="footer">
+          <p>Best regards,</p>
+          <p>MyHotel Team</p>
+        </div>
+      </div>
+    </body>
+  </html>
+  `;
 
   const mailOpstions = {
     // email admin operational
     from: "trianahilmi7@gmail.com",
     // email customer
-    to: "trianahilmi5@gmail.com",
+    to: "zikranasnawi69@gmail.com",
 
     //body email :
     subject: "your hotel booking successfuly verifited ",
